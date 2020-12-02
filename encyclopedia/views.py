aa84f9ca06f7dcd1fd618ea5b82096a3ae7ce6a5
Timothy Liu <timsgithub@gmail.com>
2020-12-02 15:14:37 -0800

    Add search bar functionality

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,11 +1,49 @@
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import render, redirect
 
 from . import util
 
+class SearchForm(forms.Form):
+    search_query = forms.CharField(label="", widget=forms.TextInput(
+        attrs={ "class": "search", "placeholder": "Search Encyclopedia"}
+    ))
 
 def index(request):
+    entries = util.list_entries()
+
+    # Handle POST request
+    if request.method == "POST":
+
+        # Get form data
+        form = SearchForm(request.POST)
+
+        # Process search query
+        if form.is_valid():
+            search_query = form.cleaned_data["search_query"]
+
+            # Redirect to entry if entry exists
+            if search_query in [entry.lower() for entry in entries]:
+                return redirect("entry", search_query)
+
+            # List entries that have the query as a subset, case-insensitive
+            else:
+                results = [entry for entry in entries if search_query in entry or search_query in entry.lower()]
+                return render(request, "encyclopedia/search.html", {
+                    "form": form,
+                    "results": results,
+                })
+
+        else:
+            # If form is invalid re-render page with existing info
+            return render(request, "encyclopedia/index.html", {
+                "form": form,
+                "entries": entries,
+            })
+
+    # Handle GET request
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "form": SearchForm(),
+        "entries": entries,
     })
 
 
@@ -17,6 +55,7 @@ def entry(request, title):
         entry = "This wiki does not have an article with this exact name."
 
     return render(request, "encyclopedia/entry.html", {
+        "form": SearchForm(),
         "title": title,
         "entry": entry,
     })
