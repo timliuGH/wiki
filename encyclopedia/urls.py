8804062e34d3b6ef35f240eb50fe55979b2a3817
Timothy Liu <timsgithub@gmail.com>
2021-01-03 08:04:00 -0800

    Added entry editing functionality

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
-    path("add_entry", views.add, name="add")
+    path("add_entry", views.add, name="add"),
+    path("edit_entry", views.edit, name="edit"),
+    path("search", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -5,69 +5,80 @@ from . import util
 
 
 class SearchForm(forms.Form):
-    search_query = forms.CharField(label="", widget=forms.TextInput(
-        attrs={ "class": "search", "placeholder": "Search Encyclopedia"}
+    query = forms.CharField(label="", widget=forms.TextInput(
+        attrs={"class": "search", "placeholder": "Search Encyclopedia"}
     ))
 
 
 class EntryForm(forms.Form):
-    title = forms.CharField()
+    title = forms.CharField(widget=forms.TextInput(
+        attrs={"autofocus": "autofocus"}
+    ))
     entry = forms.CharField(widget=forms.Textarea(
         attrs={"rows": 20, "cols": 120}
     ))
 
 
+class EditForm(forms.Form):
+    title = forms.CharField(widget=forms.HiddenInput())
+
+
 def index(request):
     entries = util.list_entries()
 
-    # Handle POST request
-    if request.method == "POST":
-
-        # Get form data
-        form_data = SearchForm(request.POST)
-
-        # Process search query
-        if form_data.is_valid():
-            search_query = form_data.cleaned_data["search_query"]
-
-            # Redirect to entry if entry exists
-            if search_query in [entry.lower() for entry in entries]:
-                return redirect("entry", search_query)
-
-            # List entries that have the query as a subset, case-insensitive
-            else:
-                # This search maintains the entry's original letter casing
-                results = [entry for entry in entries if search_query in entry or search_query in entry.lower()]
-                return render(request, "encyclopedia/search.html", {
-                    "search_form": form_data,
-                    "results": results,
-                })
-
-        else:
-            # If form is invalid re-render page with existing info
-            return render(request, "encyclopedia/index.html", {
-                "search_form": form_data,
-                "entries": entries,
-            })
-
-    # Handle GET request
     return render(request, "encyclopedia/index.html", {
         "search_form": SearchForm(),
         "entries": entries,
     })
 
 
+def search(request):
+    entries = util.list_entries()
+
+    # Get form data
+    form_data = SearchForm(request.GET)
+
+    # Process search query
+    if form_data.is_valid():
+        query = form_data.cleaned_data["query"]
+
+        # Redirect to entry if entry exists
+        for entry in entries:
+            if query.lower() == entry.lower():
+                return redirect("entry", entry)
+
+        # List entries that have the query as a subset, case-insensitive
+        # This search maintains the entry's original letter casing
+        results = [entry for entry in entries if query.lower() in entry.lower()]
+        return render(request, "encyclopedia/search.html", {
+            "search_form": SearchForm(),
+            "results": results,
+        })
+    else:
+        # If form is invalid re-render page with existing info
+        return render(request, "encyclopedia/index.html", {
+            "search_form": form_data,
+            "entries": entries,
+        })
+
+
 def entry(request, title):
     entry = util.get_entry(title)
 
     if not entry:
-        title = "Error: No Entry Found"
-        entry = "This wiki does not have an article with this exact name."
+        return render(request, "encyclopedia/noentry.html")
+
+    # Get correct casing for existing entry's title
+    entries = util.list_entries()
+    for existing_entry in entries:
+        if title.lower() == existing_entry.lower():
+            title = existing_entry
 
     return render(request, "encyclopedia/entry.html", {
         "search_form": SearchForm(),
         "title": title,
         "entry": entry,
+        "edit": EditForm({"title": title}),
     })
 
 
@@ -85,7 +96,7 @@ def add(request):
             title = form_data.cleaned_data["title"]
 
             # Find entry with same name and retrieve correct letter casing
-            existing_entry = [entry for entry in entries if title in entry or title in entry.lower()]
+            existing_entry = [entry for entry in entries if title.lower() == entry.lower()]
 
             # If entry exists, redirect to same page with error message
             if len(existing_entry) > 0:
@@ -112,8 +123,47 @@ def add(request):
                 "entry_form": form_data,
             })
 
-    # Handle GET request
+    # Handle GET request (user clicks on 'Create New Page' button)
     return render(request, "encyclopedia/add.html", {
         "search_form": SearchForm(),
         "entry_form": EntryForm(),
     })
+
+
+def edit(request):
+    # Handle POST request (user submits edits for an entry)
+    if request.method == "POST":
+
+        # Validate form data
+        form_data = EntryForm(request.POST)
+        if not form_data.is_valid():
+            return render(request, "encyclopedia/error.html")
+
+        # Parse submitted form for relevant content
+        title = form_data.cleaned_data["title"]
+        entry = form_data.cleaned_data["entry"]
+
+        # Save edits to disk
+        util.save_entry(title, entry)
+
+        # Redirect user to updated entry
+        return redirect("entry", title)
+    
+    # Handle GET request (user clicked on entry's edit button)
+    else:
+        # Validate form data and/or request is valid
+        form_data = EditForm(request.GET)
+        if not form_data.is_valid():
+            return render(request, "encyclopedia/error.html")
+        
+        # Determine which entry is being edited
+        title = form_data.cleaned_data["title"]
+
+        # Retrieve corresponding entry
+        entry = util.get_entry(title)
+
+        # Send user to new page populated with the current entry to edit
+        return render(request, "encyclopedia/edit.html", {
+            "title": title,
+            "entry_form": EntryForm({"title": title, "entry": entry})
+        })
